7dc0a6877bc6a3a7d7621e4e1ca9a68b2941b075
Rinor Komorani <komoranirinor@gmail.com>
2025-02-11 18:19:32 -0400

    finished product, added overflow checks by flashing buffer

--- Connor/Assignments/A5/word_guesser.c
+++ Connor/Assignments/A5/word_guesser.c
@@ -9,12 +9,14 @@
 #define MAXLEN 6
 #define MAXWORDS 100
 
-
 //TODO create game loop, incorporate game logic. incorportate file output. make a Makefile
 void read_words(char * filepointer, char ** buffer, int * words);
+void hint_generator(char * secret, char * guess, char * feedback);
+void write_file(FILE *file, int attempt, char * guess);
+
 int main(int argc, char ** argv)
 {
-    if(argc != 2)
+    if(argc != 3)
     {
         fprintf(stderr, "Please provide a word file, and output file.\n");
         return EXIT_FAILURE;
@@ -22,11 +24,11 @@ int main(int argc, char ** argv)
     
     srand(time(NULL));
 
-    char ** buffer = malloc(sizeof(char *) * MAXLEN);
+    char ** buffer = malloc(sizeof(char *) * MAXWORDS);
     int words = 0;
     for (int i = 0; i < MAXLEN; i++)
     {
-        *(buffer + i) = malloc(sizeof(char) * MAXWORDS);
+        *(buffer + i) = malloc(sizeof(char) * MAXLEN);
     }
 
     read_words(argv[1], buffer, &words);
@@ -39,6 +41,49 @@ int main(int argc, char ** argv)
     char * secret_word = malloc(sizeof(char) * MAXLEN);
     strcpy(secret_word, buffer[rand() % words]);
     printf("Secret word: %s\n", secret_word);
+        
+    FILE *output = fopen(argv[2], "w");
+    if (output == NULL)
+    {
+       perror("Error opening word file");
+       exit(1);
+    }
+    
+    char * guess = malloc(sizeof(char) * MAXLEN);
+    char * feedback = malloc(sizeof(char) * MAXLEN);
+    int attempt = 1;
+
+    printf("Guess the %d-letter word!\n", MAXLEN - 1);
+    
+    while(1)
+    {
+        printf("Enter a guess:\n");
+        fgets(guess, MAXLEN, stdin);
+        guess[strcspn(guess, "\n")] = '\0'; // Remove newline if present
+
+        // Flush buffer if input was too long
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF); 
+        
+        if (strlen(guess) != MAXLEN - 1)
+        {
+            printf("Please enter exactly 5 letters.\n");
+            continue;
+        }
+
+        hint_generator(secret_word, guess, feedback);
+        printf("%s\n", feedback);
+        write_file(output, attempt, guess);        
+
+        if (strcmp(feedback, "*****") == 0)
+        {
+            printf("Congratulations! You got it!\n");
+            break;
+        }
+        attempt++;
+    }
+
+    fclose(output);
     return EXIT_SUCCESS;
 }
 
@@ -61,7 +106,26 @@ void read_words(char * file, char ** buffer, int * words)
     fclose(fptr);
 }
 
-void print_words(char * file);
+void hint_generator(char * secret, char * guess, char * feedback)
+{
+    for (int i = 0; i < MAXLEN - 1; i++)
+    {
+        if (*(guess + i) == *(secret + i))
+        {
+            *(feedback + i) = '*';
+        }
+        else if (*(guess + i) < *(secret + i))
+        {
+            *(feedback + i) = '+';
+        }
+        else
+        {
+            *(feedback + i) = '-';
+        }
+    }
+
+    *(feedback + MAXLEN - 1) = '\0';
+}
 
 void eraseMemory(char ** arr, int row)
 {
@@ -79,4 +143,9 @@ void printArray(char ** arr, int row)
     {
         printf("%s\n", *(arr + i));
     }
+}
+
+void write_file(FILE *file, int attempt, char * guess)
+{
+    fprintf(file, "%d: %s\n", attempt, guess);
 }
